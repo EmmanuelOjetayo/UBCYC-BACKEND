994dd52895cd8c58b861571207fcb66904a39110
EmmanuelOjetayo <praisee827@gmail.com>
2026-08-31 13:07:16 +0100

    Update UserController.js

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -127,12 +127,7 @@ const genOTP = () => {
 
 // Nodemailer transporter
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    requireTLS: true,
-    family: 4,
-
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
